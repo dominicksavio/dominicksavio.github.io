7854687417a96dd532b37f104a86eaa1255b9867
dominic k savio <dskuncheria11@users.noreply.github.com>
2018-06-09 13:34:33 +0530

    updating button

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
 var body = document.getElementById("gradient");
+var but = document.getElementById("mc-embedded-subscribe");
 
 function setGradient() {
 	body.style.background = 
@@ -11,9 +13,11 @@ function setGradient() {
 	+ color2.value 
 	+ ")";
 
-	css.textContent = body.style.background + ";";
+	but.style.background =
+	color3.value;
+	
+	css.textContent = body.style.background + but.style.background + ";";
 }
-
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
+color3.addEventListener("input", setGradient);
